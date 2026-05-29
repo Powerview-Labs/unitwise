7c0198369970b289053c54c71a8fe24959b26283
Ifeanyi Moses <ifymmoses@Ifeanyis-Mac.local>
2026-05-29 07:06:06 -0700

    fix: upgrade Firebase to v3/v5 to fix gRPC -G flag issue

--- frontend/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
+++ frontend/ios/Flutter/ephemeral/Packages/FlutterGeneratedPluginSwiftPackage/Package.swift
@@ -16,6 +16,10 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "shared_preferences_foundation", path: "../.packages/shared_preferences_foundation-2.5.6"),
+        .package(name: "firebase_storage", path: "../.packages/firebase_storage-12.4.10"),
+        .package(name: "firebase_core", path: "../.packages/firebase_core-3.15.2"),
+        .package(name: "firebase_auth", path: "../.packages/firebase_auth-5.7.0"),
+        .package(name: "cloud_firestore", path: "../.packages/cloud_firestore-5.6.12"),
         .package(name: "FlutterFramework", path: "../.packages/FlutterFramework")
     ],
     targets: [
@@ -23,6 +27,10 @@ let package = Package(
             name: "FlutterGeneratedPluginSwiftPackage",
             dependencies: [
                 .product(name: "shared-preferences-foundation", package: "shared_preferences_foundation"),
+                .product(name: "firebase-storage", package: "firebase_storage"),
+                .product(name: "firebase-core", package: "firebase_core"),
+                .product(name: "firebase-auth", package: "firebase_auth"),
+                .product(name: "cloud-firestore", package: "cloud_firestore"),
                 .product(name: "FlutterFramework", package: "FlutterFramework")
             ]
         )
